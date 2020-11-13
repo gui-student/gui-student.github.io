fe344490a157da44d5d1f98f03c0b27b3893e7aa
gui-student <r4kgry0w6lg54ap@gmail.com>
2020-11-12 23:57:26 -0500

    Add HW-6.html

---
+++ assets/js/validate.js
@@ -0,0 +1,71 @@
+// username: gui-student
+
+$(document).ready(function(){
+
+    $("form[name='input']").validate({
+      rules: {
+        xBegin: {
+            required: true,
+            min: -50,
+            max: 50,
+            number: true,
+            integer: true,
+        },
+        xEnd: {
+            required: true,
+            min: -50,
+            max: 50,
+            number: true,
+            integer: true,
+        },
+        yBegin: {
+            required: true,
+            min: -50,
+            max: 50,
+            number: true,
+            integer: true,
+        },
+        yEnd: {
+            required: true,
+            min: -50,
+            max: 50,
+            number: true,
+            integer: true,
+        },
+      },
+
+      messages: {
+          xBegin: {
+              required: "No input given. Please enter an integer value between -50 and 50.",
+              min: "Please enter an integer value between -50 and 50.",
+              max: "Please enter an integer value between -50 and 50. ",
+              number: "Please enter an integer value between -50 and 50.",
+              integer: "Please enter an integer value between -50 and 50.",
+          },
+          xEnd: {
+              required: "No input given. Please enter an integer value between -50 and 50.",
+              min: "Please enter an integer value between -50 and 50.",
+              max: "Please enter an integer value between -50 and 50. ",
+              number: "Please enter an integer value between -50 and 50.",
+              integer: "Please enter an integer value between -50 and 50.",
+           },
+          yBegin: {
+              required: "No input given. Please enter an integer value between -50 and 50.",
+              min: "Please enter an integer value between -50 and 50.",
+              max: "Please enter an integer value between -50 and 50. ",
+              number: "Please enter an integer value between -50 and 50.",
+              integer: "Please enter an integer value between -50 and 50.",
+          },
+          yEnd: {
+              required: "No input given. Please enter an integer value between -50 and 50.",
+              min: "Please enter an integer value between -50 and 50.",
+              max: "Please enter an integer value between -50 and 50. ",
+              number: "Please enter an integer value between -50 and 50.",
+              integer: "Please enter an integer value between -50 and 50.",
+          },
+      },
+    submitHandler: function(form) {
+        generateTable();
+      }
+    });
+  });
